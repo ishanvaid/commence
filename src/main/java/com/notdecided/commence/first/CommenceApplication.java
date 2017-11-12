2b93adeafacb0ceaef1474af43cc9acabdab63ea
ishanvaid <mehta.ishan.vaid@gmail.com>
2017-11-12 23:15:37 +0530

    First commit

---
+++ src/main/java/com/notdecided/commence/first/CommenceApplication.java
@@ -0,0 +1,12 @@
+package com.notdecided.commence.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommenceApplication.class, args);
+	}
+}
